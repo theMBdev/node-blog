11d7b0955ad05dc3432c4ec129b250c77e304e22
Martin Brown <35014201+martingit9@users.noreply.github.com>
2019-01-11 04:17:08 +0000

    Content management page added, post update and delete functionality added

--- app.js
+++ app.js
@@ -40,12 +40,23 @@ MongoClient.connect('mongodb://' + userName + ':' + password + blogString, { use
 app.get('/', (req, res) => {
     db.collection('blogPosts').find().toArray((err, result) => {
         if (err) return console.log(err)
+
+        result.reverse();
         // renders index.ejs with the blogposts array
         res.render('index.ejs', {blogPosts: result})
     })
 })
 
-// post blog posts
+// Main post view
+// get individual post using posts id
+app.get('/post/:id', (req, res) => {    
+    db.collection('blogPosts').find({"_id": ObjectId(req.params.id)}).toArray((err, result) => {
+        if (err) return console.log(err)
+        res.render('post.ejs', {blogPosts: result})
+    })
+})
+
+// CREATE blog posts
 app.post('/blogPosts', (req, res) => {
     db.collection('blogPosts').insertOne(req.body, (err, result) => {
         if (err) return console.log(err)
@@ -54,20 +65,63 @@ app.post('/blogPosts', (req, res) => {
     })
 })
 
-app.get('/createPost', (req, res) => {   
-    res.render('createPost.ejs')
+// UPDATE blog posts
+app.post('/update-post/:id', (req, res) => {    
+    
+    db.collection('blogPosts').updateOne({_id: ObjectId(req.params.id)}, { $set: {title : req.body.title, post : req.body.post} },{upsert: true}, (err) => {
+        if(err){
+            console.log(err)
+            return;
+        } else {
+            res.redirect('/')
+        }
+    })
 })
+
+// DELETE blog post
+app.get('/delete/:id', (req, res) => {   
+    db.collection('blogPosts').deleteOne({"_id": ObjectId(req.params.id)}, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log('deleted from database')        
+        res.redirect('/')
+    })
+});        
+
 
 app.get('/about', (req, res) => {    
     res.render('about.ejs')
 })
 
-// get individual post using posts id
-app.get('/post/:id', (req, res) => {    
+app.get('/create-post', (req, res) => {   
+    res.render('create-post.ejs')
+})
+
+app.get('/update-post/:id', (req, res) => {
     db.collection('blogPosts').find({"_id": ObjectId(req.params.id)}).toArray((err, result) => {
         if (err) return console.log(err)
+        res.render('update-post.ejs', {blogPosts: result})
+    })  
+})   
 
-        res.render('post.ejs', {blogPosts: result})
+app.get('/about', (req, res) => {    
+    res.render('about.ejs')
+})
+
+// content management center
+app.get('/cms', (req, res) => {    
+    res.render('cms.ejs')
+})
+
+app.get('/cms-posts', (req, res) => {
+    db.collection('blogPosts').find().toArray((err, result) => {
+        if (err) return console.log(err)
+
+        result.reverse();
+        // renders index.ejs with the blogposts array
+        res.render('cms-posts.ejs', {blogPosts: result})
     })
 })
+
+
 
