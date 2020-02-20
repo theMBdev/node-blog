3a53f244018fbecb5602e0c889b7a24168622769
Martin Brown <35014201+martingit9@users.noreply.github.com>
2020-02-20 23:05:51 +0000

    heroku config passwords

---
+++ configFile.js
@@ -0,0 +1,9 @@
+// values stored on heroku
+
+exports.userName = process.env.userName;
+exports.password = process.env.password;
+exports.blogString = process.env.blogString;
+
+
+exports.emailUser = process.env.emailUser;
+exports.emailPass = process.env.emailPass;
